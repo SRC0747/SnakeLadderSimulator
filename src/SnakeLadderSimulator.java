4c3cd6331c6c2d221449e9c9bdae6abfb645ec45
SRC0747 <srcsrk1998@gmail.com>
2021-08-07 01:44:12 +0530

    added: Reporting the no. of times the dice was played to win the game and also the position after every die role.

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -5,9 +5,11 @@ public class SnakeLadderSimulator {
         int player=1;
         int position;
         int position1=0;
+        int diceRollCount=0;
         while(position1<100) {
             int randomCheck = (int) Math.floor(Math.random() + 1) % 6;
             System.out.println("Random Dice number is:" + randomCheck);
+            diceRollCount++;
             int place = (int) Math.floor(Math.random()) % 3;
             System.out.println("Position is:" + place);
             if (place == 1) {
@@ -15,15 +17,18 @@ public class SnakeLadderSimulator {
                 {
                     position1 = position1 + randomCheck;
                 }
+                System.out.println("position of die :" + position1);
             }
             if (place == 2) {
                 position1 = position1 - randomCheck;
+                System.out.println("position of die :" + position1);
                 if (position1 < 0)
                 {
                     position1 = 0;
                 }
             } else {
                 position1 = position1+0;
+                System.out.println("position of die :" + position1);
             }
             System.out.println("The player wins with the position" + position1);
         }
