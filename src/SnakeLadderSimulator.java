0bc62769daba5fb7a381646bd223c353f8232af1
SRC0747 <srcsrk1998@gmail.com>
2021-08-07 02:10:50 +0530

    added: Playing with 2 players incase 1 player gets a ladder and plays over again and finally report which player won the game.

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -2,35 +2,50 @@ public class SnakeLadderSimulator {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Snake and Ladder Siimulator problem.");
-        int player=1;
+        int player=0;
         int position;
         int position1=0;
+        int[] playerPosition = new int[2];
         int diceRollCount=0;
-        while(position1<100) {
+        while(playerPosition[player] <100) {
             int randomCheck = (int) Math.floor(Math.random() + 1) % 6;
             System.out.println("Random Dice number is:" + randomCheck);
             diceRollCount++;
             int place = (int) Math.floor(Math.random()) % 3;
             System.out.println("Position is:" + place);
             if (place == 1) {
-                if ((position1 + randomCheck) <= 100)
+                if ((playerPosition[player] + randomCheck) <= 100)
                 {
-                    position1 = position1 + randomCheck;
+                    playerPosition[player]  = playerPosition[player]  + randomCheck;
                 }
-                System.out.println("position of die :" + position1);
+                System.out.println(" player " + player + "position: " + playerPosition[player]);
             }
             if (place == 2) {
-                position1 = position1 - randomCheck;
-                System.out.println("position of die :" + position1);
-                if (position1 < 0)
+                playerPosition[player]  = playerPosition[player]  - randomCheck;
+                if (playerPosition[player]  < 0)
                 {
-                    position1 = 0;
+                    playerPosition[player] = 0;
                 }
+                System.out.println(" player " + player + "position: " + playerPosition[player]);
             } else {
-                position1 = position1+0;
-                System.out.println("position of die :" + position1);
+                System.out.println(" player " + player + "position: " + playerPosition[player]);
             }
-            System.out.println("The player wins with the position" + position1);
+            if (place == 0 || place == 2)
+            {
+                if (player == 0)
+                {
+                    player = 1;
+                }
+                else
+                {
+                    player = 0;
+                }
+            }
+
         }
+        System.out.println("The winner of snake and ladder is player" + player);
+        System.out.println("The number of times dice roll is :" + diceRollCount);
+        /// Console.WriteLine("The winning player is :" + winningPlayer);
+
     }
 }
