b1ecc7145e05047788381f504bb583eabf0a7f48
SRC0747 <srcsrk1998@gmail.com>
2021-08-06 22:40:31 +0530

    added: Getting random Dice numbers between 1 to 6 using ((RANDOM)).

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -4,5 +4,7 @@ public class SnakeLadderSimulator {
         System.out.println("Welcome to Snake and Ladder Siimulator problem.");
         int player=1;
         int position=0;
+        int randomCheck=(int) Math.floor(Math.random()+1)%6;
+        System.out.println("Random Dice number is:"+randomCheck);
     }
 }
