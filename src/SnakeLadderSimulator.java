76382fa0ab17a3dda6093d014ccf2436bcc811f5
SRC0747 <srcsrk1998@gmail.com>
2021-08-07 00:35:57 +0530

    added: The player checks that there is No play or Ladder or Snake.

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -6,5 +6,19 @@ public class SnakeLadderSimulator {
         int position=0;
         int randomCheck=(int) Math.floor(Math.random()+1)%6;
         System.out.println("Random Dice number is:"+randomCheck);
+        int place=(int) Math.floor(Math.random())%3;
+        System.out.println("Position is:"+place);
+        if(place==1)
+        {
+            System.out.println("Ladder moves ahead by the position received in the die.");
+        }
+        if(place==2)
+        {
+            System.out.println("Snake moves behind by the position received in the die.");
+        }
+        else
+        {
+            System.out.println("The player stays on the same position.");
+        }
     }
 }
