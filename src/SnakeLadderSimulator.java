b5c522b805a2c3ecccf1838fab4f38b9f6e43a33
SRC0747 <srcsrk1998@gmail.com>
2021-08-06 19:25:58 +0530

    added: Single player starts to play the game at starting position 0.

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -2,5 +2,7 @@ public class SnakeLadderSimulator {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Snake and Ladder Siimulator problem.");
+        int player=1;
+        int position=0;
     }
 }
